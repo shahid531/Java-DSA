d62401fd47c4a440a3749bd28ce3745d398e788a
Saheed Ansari <shahidansari531@gmail.com>
2025-05-24 23:16:44 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+import static array.ArrayEasy.plusOne;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {9, 9, 9};
+
+        System.out.println(Arrays.toString(plusOne(arr1)));
+    }
+}
