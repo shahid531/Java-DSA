1bbc474c7ca9776f23ed21780a63a1676c87805c
Saheed Ansari <shahidansari531@gmail.com>
2025-05-25 07:52:24 +0530

    Two Sum

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 
-import static array.ArrayEasy.plusOne;
+import static array.ArrayEasy.twoSum;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {9, 9, 9};
+        int[] arr1 = {3, 3};
+        int target = 6;
 
-        System.out.println(Arrays.toString(plusOne(arr1)));
+        System.out.println(Arrays.toString(twoSum(arr1, target)));
     }
 }
--- array/ArrayEasy.java
+++ array/ArrayEasy.java
@@ -3,6 +3,18 @@ package array;
 import java.util.Arrays;
 
 public class ArrayEasy {
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
     public static int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
