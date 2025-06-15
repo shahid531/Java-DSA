9057b3860ad2cfe7dcfd4e9db88608a24aedd953
Saheed Ansari <shahidansari531@gmail.com>
2025-06-15 22:51:05 +0530

    Valid Anagram

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
-
-import static hashing.HashingEasy.twoSum;
+import static hashing.HashingEasy.isAnagram;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {2, 7, 11, 15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(arr1, target)));
+        String s = "rat";
+        String t = "car";
+        System.out.println(isAnagram(s,t));
     }
 }
--- hashing/HashingEasy.java
+++ hashing/HashingEasy.java
@@ -1,5 +1,6 @@
 package hashing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class HashingEasy {
@@ -15,5 +16,24 @@ public class HashingEasy {
             hashMap.put(nums[i], i);
         }
         return new int[]{};
+    }
+
+    // https://leetcode.com/problems/valid-anagram/description/
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+        }
+        System.out.println(hashMap);
+
+        for (char c : t.toCharArray()) {
+            if (!hashMap.containsKey(c) || hashMap.get(c) == 0) return false;
+            hashMap.put(c, hashMap.get(c) - 1);
+        }
+
+        System.out.println(hashMap);
+
+        return true;
     }
 }
