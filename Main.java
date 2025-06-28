cf32dccb94796eb1c9de1ac8f6b4b14a98cba55b
Saheed Ansari <shahidansari531@gmail.com>
2025-06-28 22:23:12 +0530

    Two Pointer Two Sum II - Input Array Is Sorted

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
-import static hashing.HashingMedium.subarraySum;
+import java.util.Arrays;
+
+import static two_pointer.TwoPointer.twoSum;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1, 2,3};
-        int k = 3;
-        System.out.println(subarraySum(nums, k));
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 }
