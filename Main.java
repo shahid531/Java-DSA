dda21ad2191f47d9290d3ea0c9d3b1984af5badc
Saheed Ansari <shahidansari531@gmail.com>
2025-07-03 21:14:40 +0530

    Two Pointer Valid Palindrome

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-import static two_pointer.TwoPointer.reverseString;
+import static two_pointer.TwoPointer.isPalindrome;
 
 public class Main {
     public static void main(String[] args) {
-        char[] chars = {'H', 'a', 'n', 'n', 'a', 'h'};
-        reverseString(chars);
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
     }
 }
