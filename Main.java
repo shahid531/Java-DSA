81e407ee04bb17a81202669f61d748c9585baa34
Saheed Ansari <shahidansari531@gmail.com>
2025-07-05 22:55:02 +0530

    Two Pointer 3Sum

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
-import static two_pointer.TwoPointer.merge;
+import java.util.List;
+
+import static two_pointer.TwoPointer.threeSum;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 0, 0, 0};
-        int[] arr2 = {2, 5, 6};
-        merge(arr1, 3, arr2, 3);
+        int[] arr1 = {-1, 0, 1, 2, -1, -4};
+        threeSum(arr1);
+
+        List<List<Integer>> triplets = threeSum(arr1);
+        for (List<Integer> triplet : triplets) {
+            System.out.println(triplet);
+        }
     }
 }
--- two_pointer/TwoPointer.java
+++ two_pointer/TwoPointer.java
@@ -1,6 +1,8 @@
 package two_pointer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TwoPointer {
     public static int[] twoSum(int[] numbers, int target) {
@@ -125,5 +127,40 @@ public class TwoPointer {
             p2--;
             p--;
         }
+    }
+
+    // https://leetcode.com/problems/3sum/description/
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Step 1: Sort the array
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Move left and right, skipping duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++; // Need bigger sum
+                } else {
+                    right--; // Need smaller sum
+                }
+            }
+        }
+
+        return result;
     }
 }
